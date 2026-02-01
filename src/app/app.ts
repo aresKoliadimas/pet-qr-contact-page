68eddb1baf9018245b64affd82e3da94af27fbae
Ares Koliadimas <ariskoliadimas@gmail.com>
2026-02-01 15:46:40 +0200

    make the socials redirect buttons
    - refactor the data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialMedia } from '../interfaces/social.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,27 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './app.scss',
 })
 export class App {
-  public readonly info = {
-    petName: 'Majin Zak',
-    homeArea: 'Thessaloniki, Analipsi',
-    phone: '+306947627851',
-    socials: [
-      {
-        id: 'instagram',
-        iconPath: '/assets/instagram.png',
-        altText: 'Instagram icon',
-        handler: '@kukunares',
-      },
-      {
-        id: 'facebook',
-        iconPath: '/assets/facebook.png',
-        altText: 'Facebook icon',
-        handler: '/kukunaress',
-      },
-    ],
-  };
-
-  public callMe(): void {
-    window.open(`tel:${encodeURIComponent(this.info.phone)}`, '_self');
-  }
+  public readonly petName: string = 'Majin Zak';
+  public readonly homeArea: string = 'Thessaloniki, Analipsi';
+  public readonly phoneNumber: string = '+306947627851';
+  public readonly socials: SocialMedia[] = [
+    {
+      id: 'instagram',
+      iconPath: '/assets/instagram.png',
+      altText: 'Instagram icon',
+      handler: '@kukunares',
+      socialMediaUrl: 'https://www.instagram.com/kukunares',
+    },
+    {
+      id: 'facebook',
+      iconPath: '/assets/facebook.png',
+      altText: 'Facebook icon',
+      handler: '/kukunaress',
+      socialMediaUrl: 'https://www.facebook.com/kukunaress',
+    },
+  ];
 }
---
+++ src/interfaces/social.interface.ts
@@ -0,0 +1,7 @@
+export interface SocialMedia {
+  id: string;
+  iconPath: string;
+  altText: string;
+  handler: string;
+  socialMediaUrl: string;
+}
